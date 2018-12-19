edcd29516b65ece06413927a931260d220bd53af
inklusiv1 <45459165+inklusiv1@users.noreply.github.com>
2018-12-19 11:21:51 +0100

    Update telnet.sh

--- telnet.sh
+++ telnet.sh
@@ -4,10 +4,8 @@ yum install telnet-server telnet
 # configure firewall
 firewall-cmd --add-service=telnet --zone=public
 firewall-cmd --add-service=telnet --zone=public --permanent
-
 # enable telnet
 echo "disable = no" > /etc/xinetd.d/telnet
-
 # restart telnet
 systemctl start telnet.socket
 systemctl enable telnet.socket
