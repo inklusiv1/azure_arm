7db7e8989b2e6cb622dedc3e09cf409d8fc9be9b
inklusiv1 <45459165+inklusiv1@users.noreply.github.com>
2018-12-19 09:28:15 +0100

    Add files via upload

---
+++ telnet.sh
@@ -0,0 +1,20 @@
+yum install telnet-server telnet
+firewall-cmd --add-service=telnet --zone=public
+firewall-cmd --add-service=telnet --zone=public --permanent
+systemctl start telnet.socket
+
+
+
+#!/bin/bash
+yum install telnet-server telnet
+
+# configure firewall
+firewall-cmd --add-service=telnet --zone=public
+firewall-cmd --add-service=telnet --zone=public --permanent
+
+# enable telnet
+echo "disable = no" > /etc/xinetd.d/telnet
+
+# restart telnet
+systemctl start telnet.socket
+systemctl enable telnet.socket
