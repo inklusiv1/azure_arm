b68330544edf659a1d679ee0d0113ea3df50514b
inklusiv1 <45459165+inklusiv1@users.noreply.github.com>
2021-03-08 15:54:00 +0100

    Update install_ad.ps1

--- install_ad.ps1
+++ install_ad.ps1
@@ -1,4 +1,5 @@
 Add-WindowsFeature AD-Domain-Services -IncludeManagementTools
-$Password = Read-Host -Prompt 'Enter SafeMode Admin Password' -AsSecureString
+$Password = $args[0]
+$dn= $args[1]
 Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath C:\Windows\NTDS -DomainName $dn -InstallDns:$true -LogPath C:\Windows\NTDS -NoRebootOnCompletion:$true -SafeModeAdministratorPassword $Password -SysvolPath C:\Windows\SYSVOL -Force:$true
 Copy-Item C:\windows\PolicyDefinitions C:\Windows\SYSVOL\sysvol\$dn\Policies\ -Recurse -force
