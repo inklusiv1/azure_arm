0f901fe157049cfb07cb5be8d00c271435a08aca
inklusiv1 <45459165+inklusiv1@users.noreply.github.com>
2021-03-08 16:32:54 +0100

    Update Install_ad.ps1

--- Install_ad.ps1
+++ Install_ad.ps1
@@ -1,5 +1,9 @@
+$CurrentDate = (Get-Date).ToString('MM-dd-yyyy_hh-mm-ss')
+$logpath="c:\Windows\Temp"
+Start-Transcript $logpath\IIS-Install-$CurrentDate.txt
 Add-WindowsFeature AD-Domain-Services -IncludeManagementTools
 $Password = $args[0]
 $dn= $args[1]
 Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath C:\Windows\NTDS -DomainName $dn -InstallDns:$true -LogPath C:\Windows\NTDS -NoRebootOnCompletion:$true -SafeModeAdministratorPassword $Password -SysvolPath C:\Windows\SYSVOL -Force:$true
 Copy-Item C:\windows\PolicyDefinitions C:\Windows\SYSVOL\sysvol\$dn\Policies\ -Recurse -force
+Stop-Transcript
